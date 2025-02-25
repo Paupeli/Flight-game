8d4481c184736053c776f460f77597f92a7be830
Ousalou <osalonen@icloud.com>
2025-02-25 13:49:25 +0200

    New user -toiminnon tavoitteiden hahmottelua

--- new_user_and_save_user.py
+++ new_user_and_save_user.py
@@ -1,5 +1,8 @@
-# Tämän koodin tarkoituksena on
+# Tämän ohjelman tarkoituksena on
 # 1) antaa käyttäjälle mahdollisuus valita uusi tai olemassa oleva käyttäjä
 # 2) luoda uusi käyttäjä
 # 3) tallentaa uusi käyttäjä tai hakea vanhan käyttäjän tiedot
 # 4) luoda mekanismi tallentamaan käyttäjän pelidata / highscore systeemiin > scoreboardin printtaus
+
+# Mietinnän alla:
+# "Lukitut käyttäjät", joita ei voida valita mutta joilla on läsnäolo scoreboardissa? Kilpailun olon luomiseksi :)
