7719d14c8cd39c51912215b0121211a26a30d682
Ousalou <osalonen@icloud.com>
2025-02-25 13:34:10 +0200

    Master dokuun ohjeita

---
+++ master_flight_game.py
@@ -0,0 +1,4 @@
+# Pushataan tänne vain valmista (tai melkein valmista) koodia, testata voi vapaasti
+# Aluksi saattaa olla ettei mikään vielä toimi, ok tehtä "raakile"
+
+# Muista kommentoida koodia !
