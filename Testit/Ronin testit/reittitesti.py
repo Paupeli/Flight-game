06c47386bb20316d27d74e278ec0a324d13a9d69
rev2103 <roni.minkkinen@gmail.com>
2025-02-27 12:58:53 +0200

    Koodi laitettu kokonaan englanniksi

--- Testit/Ronin testit/reittitesti.py
+++ Testit/Ronin testit/reittitesti.py
@@ -11,25 +11,25 @@ import random
 route_length = int(input("Give the desired length of the route in numbers: "))
 country_list = []
 airport_list = []
-def lentokentan_valitsin():
-    numero = random.randint(1, 5000)
-    sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {numero} and airport.type = 'large_airport';"
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
-    while kursori.rowcount == 0:
-        numero = random.randint(1, 5000)
-        sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {numero} and airport.type = 'large_airport';"
-        kursori = yhteys.cursor()
-        kursori.execute(sql)
-        tulos = kursori.fetchall()
+def route_creator():
+    num = random.randint(1, 5000)
+    sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    tulos = cursor.fetchall()
+    while cursor.rowcount == 0:
+        num = random.randint(1, 5000)
+        sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
+        cursor = yhteys.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
     else:
-        for rivi in tulos:
-            airport_list.append(rivi[0])
-            country_list.append(rivi[1])
+        for row in result:
+            airport_list.append(row[0])
+            country_list.append(row[1])
     return
 while route_length > len(country_list):
-    lentokentan_valitsin()
+    route_creator()
 else: print(country_list)
 print(airport_list)
 
