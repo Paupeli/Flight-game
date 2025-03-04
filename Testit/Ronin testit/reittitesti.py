3166bf557a2bda1300b8a06ea70d524262839539
rev2103 <roni.minkkinen@gmail.com>
2025-03-04 09:57:14 +0200

    Väärien maiden listan tekevän koodi tehty. (Ei testattu koska läppäri jäätyy kun pyöritän sitä, mutta pitäisi toimia :D)

--- Testit/Ronin testit/reittitesti.py
+++ Testit/Ronin testit/reittitesti.py
@@ -6,9 +6,13 @@ yhteys = mysql.connector.connect(
     user='testaaja',
     password='j33s',
     autocommit=True,
+    #remember to add collation here if you are not on linux!
 )
 import random
-route_length = int(input("Give the desired length of the route in numbers: "))
+route_length = int(input("Give the desired length of the route in numbers (max 20): "))
+if route_length > 20:
+    print("The desired length is too long")
+    route_length = int(input("Give the desired length of the route in numbers (max 20): "))
 country_list = []
 airport_list = []
 def route_creator():
@@ -16,7 +20,7 @@ def route_creator():
     sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
     cursor = yhteys.cursor()
     cursor.execute(sql)
-    tulos = cursor.fetchall()
+    result = cursor.fetchall()
     while cursor.rowcount == 0:
         num = random.randint(1, 5000)
         sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
@@ -32,4 +36,39 @@ while route_length > len(country_list):
     route_creator()
 else: print(country_list)
 print(airport_list)
+
+wrong_country_list = []
+def country_selector_for_questions():
+    num = random.randint(1, 5000)
+    sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    while cursor.rowcount == 0:
+        num = random.randint(1, 5000)
+        sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
+        cursor = yhteys.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    else:
+        for row in result:
+            while row[1] not in country_list:
+                wrong_country_list.append(row[1])
+            else:
+                num = random.randint(1, 5000)
+                sql = f"select airport.name, country.name from airport inner join country on airport.iso_country = country.iso_country and airport.id = {num} and airport.type = 'large_airport';"
+                cursor = yhteys.cursor()
+                cursor.execute(sql)
+                result = cursor.fetchall()
+    return
+while route_length * 3 > len(wrong_country_list):
+    country_selector_for_questions()
+else: print(wrong_country_list)
+
+
+
+
+
+
+
 
