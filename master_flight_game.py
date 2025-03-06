fc5ea79b6b2b1ef609b74e68dfa8aca750247a3c
Ousalou <osalonen@icloud.com>
2025-03-06 15:20:57 +0200

    Main menu lisätty masteriin

--- master_flight_game.py
+++ master_flight_game.py
@@ -170,6 +170,138 @@ def score_board_print():
         # >> Luo uusi hahmo
             # >>> Valitse, kuinka pitkä peli (**RONI**)
 
+
+#HUOM! NÄMÄ OVAT FUNKTIOT, JOTKA KÄYNNISTYVÄT ALLA MAIN MENUN PÄÄKOODIN TOIMESTA (MERKITTY MISTÄ ALKAA)
+
+def main_menu(menu_selection):
+    print("\n-------------------------\n")
+    main_menu_text = """▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+▐ ███╗   ███╗ █████╗ ██╗███╗   ██╗    ███╗   ███╗███████╗███╗   ██╗██╗   ██╗ ▌
+▐ ████╗ ████║██╔══██╗██║████╗  ██║    ████╗ ████║██╔════╝████╗  ██║██║   ██║ ▌
+▐ ██╔████╔██║███████║██║██╔██╗ ██║    ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║ ▌
+▐ ██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║ ▌
+▐ ██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝ ▌
+▐ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝  ▌
+▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+                                                                          """
+    print(main_menu_text)
+    print("-------------------------\n")
+    print(f"\n{(menu_selection)}\n") #Muotoillaan nää vielä listaksi?
+    option = input(">>> Write down your selection: ").lower()
+    return option
+
+
+def main_menu_options(option):                                                                  #TÄMÄ PALAUTTAA USERIN PELIN ALOITTAMISEKSI
+    global user
+    while True:
+        if option == "new game":
+            user = new_game()
+            break
+        elif option == "scoreboard":
+            scoreboard()
+            main_menu(menu_selection)
+        elif option == "instructions":
+            instructions()
+            main_menu(menu_selection)
+        elif option == "quit game":
+            quit()
+    return user         # WHILE LOOP EI ETENE ENNEN KUN def new_game() PALAUTTAA user-arvon
+
+
+def new_game():                                                                                #TÄMÄ ON VALIKKO UUDEN PELIN LUOMISEKSI
+        # options = ['Old user', 'New user']
+    global user
+    option = input("Do you want to play as an old user or create a new user? ").lower()
+    if option == "old user":
+        user = old_user()
+    elif option == "new user":
+        user = new_user()
+    return user              #PALAUTTAA user-arvon AIEMMALLE FUNKTIOLLE main_menu_options
+
+def old_user():
+    def all_users_fetch():
+        sql = "select screen_name from game;"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        users = kursori.fetchall()
+        print("\nExcisting users:")
+        for user in users:
+            print(user)
+        return
+    users = all_users_fetch()                                                                   #Tämä on vähän höpö funktio, että voidaan listata olemassa olevat käyttävät old_user funktiossa
+
+    while True:
+        user = input("Which user would you like to choose? ")
+        sql1 = f"select screen_name from game where screen_name = '{user}';"
+        cursor = yhteys.cursor()
+        cursor.execute(sql1)
+        result = cursor.fetchall()
+        if not result:                                                  #Tarkistaa että käyttäjä on olemassa, muute while-loop jatkuu
+            print("Please select an existing user.")
+            users = all_users_fetch()
+        else:
+            break
+    # !!!! TÄHÄN VIELÄ SIJAINNIN NOLLAUS HELSINKIIN kun uusi tietokanta
+    return user
+
+def new_user():                                                                                        #Pycharm väittää että on unreachable, not true
+    while True:
+        user = input("What is your username? ")
+        sql1 = f"select screen_name from game where screen_name = '{user}';"
+        cursor = yhteys.cursor()
+        cursor.execute(sql1)
+        result = cursor.fetchall()
+        if cursor.rowcount == 0:
+            cursor.execute("SELECT COALESCE(MAX(id), 0) + 1 FROM game;")
+            next_id = cursor.fetchone()[0]
+
+            sql = f"INSERT INTO game (id, screen_name, location) VALUES ({next_id}, '{user}');" #({next_id}, '{user}', EFHK) !!!!!!
+                #entä pisteet?
+            kursori = yhteys.cursor()
+            kursori.execute(sql)
+            print("User created.\nProceeding to the game...\n----------")
+            break
+        else:
+            print("User already exists. Please type in a new username.")
+                # tähän vois jotenkin keksiä, voisko palata tonne hahmovalintaan?
+    return user
+
+
+def scoreboard():
+    sql = f"select screen_name, co2_consumed from game order by co2_consumed desc limit 5;"                                 #NYT CO2 CONSUMED !!!!! VAIHDA "SCORE tms"
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print (f"\n______________________________\n{'USER':<15} | {'HIGHSCORE':<10} |\n_______________________________")
+    for row in result:
+        screen_name = row[0] if row[0] is not None else "N/A"
+        score = row[1] if row[1] is not None else "N/A"
+        print(f"{screen_name:<15} | {score:<10} |\n______________________________")                    #TÄMÄ PRINTTAA TAULUKON pisteistä, älä sörki muotoilua jos et näe mitä teet :D
+    return
+
+def quit_game():
+    print("-----------\nQuitting game...\n____________")
+    exit()
+
+
+def instructions():
+    print(
+        "___________\n\n\nWelcome to Across Europe!\nThe rules are simple:\n\n 1) You start in Helsinki Vantaa Airport\n\n 2) You get a clue where you should go next"
+        "\n  - Right guess: +100 points\n  - Wrong guess: -50 points\n  - Guess wrong 3 times: game over!\n\n 3) Once you get to the correct airport, you get a task\n  - Complete the task and get +50 points!\n"
+        "  - Fail the task, shame on you, -25 points \n\n 4) Finish the game by arriving to the final airport! :)")
+    return
+
+
+menu_selection = ['New Game', 'Scoreboard', 'Instructions', 'Quit Game']
+
+# TÄMÄ ALLA OLEVA ON **MAIN MENUN PÄÄKOODI**, JOKA KÄYNNISTÄÄ FUNKTIOT YLLÄ!!!
+
+option = main_menu(menu_selection)
+user = main_menu_options(option)
+
+# ^^^^^^^^^^
+
+
 route_length = int(input("Give the desired length of the route in numbers (max 20): "))
 if route_length > 20:
     print("The desired length is too long")
