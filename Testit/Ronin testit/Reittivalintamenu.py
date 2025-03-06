c456d235d2e03b2d68b0fdd43b25acaed0cf517e
rev2103 <roni.minkkinen@gmail.com>
2025-03-06 14:59:21 +0200

    Kerroin lisätty

--- Testit/Ronin testit/Reittivalintamenu.py
+++ Testit/Ronin testit/Reittivalintamenu.py
@@ -4,3 +4,10 @@ while True:
         break
     else:
         print("Please enter a valid route length")
+mult = 0
+if route_length == 5:
+    mult = 1.5
+elif route_length == 10:
+    mult = 1
+elif route_length == 15:
+    mult = 0.5
