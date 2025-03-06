16e41705125020a6f9acbf54d5762b3f20d00867
sonjameskus <sonja.meskus@metropolia.fi>
2025-03-06 14:51:51 +0200

    tekstit maavihjeiden yhteydessä

---
+++ sonjan testi/clues.py
@@ -0,0 +1,5 @@
+#"Choose your next destination (A, B, C):" <<< after clue, user input
+
+#"Amazing! You travelled to the correct place. Here's your task:" <<< if correct, after give player their task
+
+#"Oh no! You travelled to the wrong place. Here's your next clue:" <<< if incorrect, after give player next (different) clue
