a36ad7517b3d7e41cf342b3b8bbc4195727db646
jpohja <pohja.johanna@gmail.com>
2025-03-08 11:26:05 +0200

    taustamusiikki

---
+++ taustamusa.py
@@ -0,0 +1,8 @@
+# plays music in the background, import taustamusa in master,  change actual music file
+
+from just_playback import Playback
+playback = Playback()
+playback.load_file('music-files/sample.mp3')
+
+playback.play()
+
