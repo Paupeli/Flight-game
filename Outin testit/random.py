216d2223fc7e97710c9e00826b0bb7274d9307c1
Ousalou <osalonen@icloud.com>
2025-03-10 14:09:03 +0200

    Main menua korjattu

--- Outin testit/random.py
+++ Outin testit/random.py
@@ -1,4 +1,3 @@
-
 import mysql.connector
 
 yhteys = mysql.connector.connect(
@@ -12,12 +11,128 @@ yhteys = mysql.connector.connect(
 
 )
 
-sql = f"select screen_name, high_score from game order by high_score desc limit 5;"
-cursor = yhteys.cursor()
-cursor.execute(sql)
-result = cursor.fetchall()
-print (f"\n______________________________\n{'USER':<15} | {'HIGH SCORE':<10} |\n_______________________________")
-for row in result:
-    screen_name = row[0] if row[0] is not None else "N/A"
-    high_score = row[1] if row[1] is not None else "N/A"
-    print(f"{screen_name:<15} | {high_score:<10} |\n______________________________")
+def main_menu(menu_selection):
+    print("\n-------------------------\n")
+    main_menu_text = """▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+▐ ███╗   ███╗ █████╗ ██╗███╗   ██╗    ███╗   ███╗███████╗███╗   ██╗██╗   ██╗ ▌
+▐ ████╗ ████║██╔══██╗██║████╗  ██║    ████╗ ████║██╔════╝████╗  ██║██║   ██║ ▌
+▐ ██╔████╔██║███████║██║██╔██╗ ██║    ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║ ▌
+▐ ██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║ ▌
+▐ ██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝ ▌
+▐ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝  ▌
+▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+                                                                          """
+    print(main_menu_text)
+    print("-------------------------\n")
+    print(f"\n{(menu_selection)}\n") #Muotoillaan nää vielä listaksi?
+    option = input(">>> Write down your selection: ").lower()
+    return option
+
+
+def main_menu_options(option):
+    while True:                                     #Looppaa main menuun kunnes pelaaja haluaa alottaa uuden pelin
+        option = main_menu(menu_selection)
+        if option == "new game":
+            user = new_game()
+            break
+        elif option == "scoreboard":
+            scoreboard()
+        elif option == "instructions":
+            instructions()
+        elif option == "quit game":
+            quit()
+    return user
+
+
+def new_game():
+        # options = ['Old user', 'New user']
+    global user
+    option = input("Do you want to play as an old user or create a new user? ").lower()
+    if option == "old user":
+        user = old_user()
+    elif option == "new user":
+        user = new_user()
+    return user
+
+def old_user():
+    def all_users_fetch():
+        sql = f"select screen_name from game;"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        users = kursori.fetchall()
+        print("\nExcisting users:")
+        for user in users:
+            print(user)
+        return                                  #Haetaan käyttäjät
+
+    users = all_users_fetch()
+
+    while True:                                 #Valitaan millä käyttäjällä aletaan pelata
+        user = input("Which user would you like to choose? ")
+        sql1 = f"select screen_name from game where screen_name = '{user}';"
+        kursori = yhteys.cursor()
+        kursori.execute(sql1)
+        result = kursori.fetchall()
+        if not result:
+            print("Please select an existing user.")
+            users = all_users_fetch()
+        else:
+            break
+        #TÄHÄN TULEE VIELLÄ VANHAN KÄYTTÄJÄN DATAN NOLLAUS
+    return user
+
+def new_user():
+    while True:
+        user = input("What is your username? ")
+        kursori = yhteys.cursor()
+        sql_check = f"SELECT screen_name FROM game WHERE screen_name = '{user}';"
+        kursori.execute(sql_check)
+        result = kursori.fetchall()
+
+        if not result:
+            new_id = "SELECT COALESCE(MAX(id), 0) + 1 FROM game;"                                   #Tässä oma uus id pelaajille, joka on +1 edellisestä
+            kursori = yhteys.cursor()
+            kursori.execute(new_id)
+            next_id = kursori.fetchone()[0]
+
+            sql_add = f"INSERT INTO game (id, location,screen_name, score, high_score) VALUES ('{next_id}', 'EFHK', '{user}', 0, 0);"   #Aloituskenttä on HKI!
+            kursori = yhteys.cursor()
+            kursori.execute(sql_add)
+            print("User created.\nProceeding to the game...\n----------")
+            break
+        else:
+            print("User already exists. Please type in a new username.")
+    return user
+
+
+def scoreboard():
+    sql = f"select screen_name, high_score from game order by high_score desc limit 5;"
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print (f"\n______________________________\n{'USER':<15} | {'HIGH SCORE':<10} |\n_______________________________")
+    for row in result:
+        screen_name = row[0] if row[0] is not None else "N/A"
+        high_score = row[1] if row[1] is not None else "N/A"
+        print(f"{screen_name:<15} | {high_score:<10} |\n______________________________")                        #Printtaa feikki-taulukon, älä sorki jos et näe mitä teet
+    return
+
+def quit_game():
+    print("-----------\nQuitting game...\n____________")
+    exit()
+
+
+def instructions():
+    print(
+        "___________\n\n\nWelcome to Across Europe!\nThe rules are simple:\n\n 1) You start in Helsinki Vantaa Airport\n\n 2) You get a clue where you should go next"
+        "\n  - Right guess: +100 points\n  - Wrong guess: -50 points\n  - Guess wrong 3 times: game over!\n\n 3) Once you get to the correct airport, you get a task\n  - Complete the task and get +50 points!\n"
+        "  - Fail the task, shame on you, -25 points \n\n 4) Finish the game by arriving to the final airport! :)")
+    return
+
+
+menu_selection = ['New Game', 'Scoreboard', 'Instructions', 'Quit Game']
+
+## NÄMÄ ALLA KÄYNNISTÄÄ FUNKTIOT YLLÄ
+option = main_menu(menu_selection)
+
+user = main_menu_options(option)
