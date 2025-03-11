288b4ad3412b03ae89b8acd05b74d65837809bd4
Paupeli <pauliina.helle@metropolia.fi>
2025-03-11 20:01:43 +0200

    tehtäväkoodi korjattu

--- master_flight_game.py
+++ master_flight_game.py
@@ -327,6 +327,10 @@ def ask_task(task, option_a, option_b, option_c, correct_answer,):
 
     user_answer = input("Enter your answer (a,b, or c): ").lower()
 
+    if user_answer not in ['A', 'B' or 'C']:
+        print("You didn't give your answer as A, B or C")
+        answer = input("Give your answer as A, B or C ").upper()
+
     if user_answer == correct_answer.lower():
         points = points + (50*mult)
         print("Correct")
@@ -334,10 +338,6 @@ def ask_task(task, option_a, option_b, option_c, correct_answer,):
         print(f"Your points: {points}")
         if count != route_length:
             print(f"Here is a clue to your next destination: ")
-
-    elif user_answer not in  ['A', 'B' or 'C']:
-        print("You didn't give your answer as A, B or C")
-        answer = input("Give your answer as A, B or C ").upper()
 
 
     else:
