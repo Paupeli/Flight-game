de1bbf578061362ca739e64e7040aec33eca7a95
Paupeli <pauliina.helle@metropolia.fi>
2025-03-11 15:11:17 +0200

    tehtäväkoodi korjattu

--- master_flight_game.py
+++ master_flight_game.py
@@ -23,7 +23,7 @@ playback = Playback()
 playback.load_file('musa.mp3')
 
 playback.play()
-#from defer import return_value
+from defer import return_value
 
 #pyfiglet, creates slanted text etc
 import pyfiglet                             #Muista asentaa python packages, uusin versio
@@ -35,7 +35,7 @@ yhteys = mysql.connector.connect(
     user='keltanokat',
     password='lentopeli',
     autocommit=True,
-    #collation='utf8mb3_general_ci'
+    collation='utf8mb3_general_ci'
 
 )
 # 0 B ) IMPORTIT TÄHÄN (import.random, jne)
@@ -334,6 +334,10 @@ def ask_task(task, option_a, option_b, option_c, correct_answer,):
         print(f"Your points: {points}")
         if count != route_length:
             print(f"Here is a clue to your next destination: ")
+
+    elif user_answer not in  ['A', 'B' or 'C']:
+        print("You didn't give your answer as A, B or C")
+        answer = input("Give your answer as A, B or C ").upper()
 
 
     else:
